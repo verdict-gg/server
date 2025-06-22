f60e81c21ccfc85c4a69ea046a816530fc10f84c
golden-hamster <isack19999@gmail.com>
2025-06-22 15:47:48 +0900

    Git Init

---
+++ src/main/java/com/verdict/verdict/VerdictApplication.java
@@ -0,0 +1,13 @@
+package com.verdict.verdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdictApplication.class, args);
+	}
+
+}
